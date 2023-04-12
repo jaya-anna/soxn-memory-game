b665222108d91481b9a923a99be74558c299e96b
Anna Hartmann <anna.ig.hartmann@gmail.com>
2023-04-12 22:29:44 +0200

    mvp done, css and styling, bug restart

---
+++ app.js
@@ -0,0 +1,146 @@
+document.addEventListener("DOMContentLoaded", () => {
+  //card options
+  const cardArray = [
+    {
+      name: "leo-white",
+      img: "images/image1.jpeg",
+    },
+    {
+      name: "leo-white",
+      img: "images/image1.jpeg",
+    },
+    {
+      name: "leo-yellow",
+      img: "images/image2.jpeg",
+    },
+    {
+      name: "leo-yellow",
+      img: "images/image2.jpeg",
+    },
+    {
+      name: "leo-red",
+      img: "images/image3.jpeg",
+    },
+    {
+      name: "leo-red",
+      img: "images/image3.jpeg",
+    },
+    {
+      name: "banana",
+      img: "images/image4.jpeg",
+    },
+    {
+      name: "banana",
+      img: "images/image4.jpeg",
+    },
+
+    {
+      name: "skate",
+      img: "images/image7.jpeg",
+    },
+    {
+      name: "skate",
+      img: "images/image7.jpeg",
+    },
+    {
+      name: "wave",
+      img: "images/image8.jpeg",
+    },
+    {
+      name: "wave",
+      img: "images/image8.jpeg",
+    },
+  ];
+
+  cardArray.sort(() => 0.5 - Math.random());
+
+  const grid = document.querySelector(".grid");
+  const resultDisplay = document.querySelector("#result");
+  const gameText = document.querySelector(".info-text");
+  const restartBtn = document.querySelector(".restart");
+
+  let cardsChosen = [];
+  let cardsChosenId = [];
+  let cardsWon = [];
+
+  // create board
+  function createBoard() {
+
+    for (let i = 0; i < cardArray.length; i++) {
+      const card = document.createElement("img");
+      card.setAttribute("src", "images/image-blank.jpg");
+      card.setAttribute("data-id", i);
+      card.setAttribute("class", "card");
+      card.addEventListener("click", flipCard);
+      grid.appendChild(card);
+    }
+    gameText.textContent = "Start playing!";
+    resultDisplay.textContent = "0 %";
+  }
+
+  // check for matches
+  function checkForMatch() {
+    const cards = document.querySelectorAll(".card");
+    const optionOneId = cardsChosenId[0];
+    const optionTwoId = cardsChosenId[1];
+
+    if (optionOneId == optionTwoId) {
+      cards[optionOneId].setAttribute("src", "images/image-blank.jpg");
+      cards[optionTwoId].setAttribute("src", "images/image-blank.jpg");
+      gameText.textContent = "You have clicked the same image!";
+    } else if (cardsChosen[0] === cardsChosen[1]) {
+      gameText.textContent = "You found a match!";
+      cards[optionOneId].setAttribute("src", "images/background.jpg");
+      cards[optionTwoId].setAttribute("src", "images/background.jpg");
+      cards[optionOneId].removeEventListener("click", flipCard);
+      cards[optionTwoId].removeEventListener("click", flipCard);
+      cardsWon.push(cardsChosen);
+      const currentScore = cardsWon.length * 16.66;
+    resultDisplay.textContent = Math.floor(currentScore) + " %"
+    } else {
+      cards[optionOneId].setAttribute("src", "images/image-blank.jpg");
+      cards[optionTwoId].setAttribute("src", "images/image-blank.jpg");
+      gameText.textContent = "Sorry, try again.";
+    }
+    cardsChosen = [];
+    cardsChosenId = [];
+   
+    if (cardsWon.length === cardArray.length / 2) {
+      grid.innerHTML = "";
+      gameText.textContent = "Congratulations! You found them all! ";
+      resultDisplay.textContent = "100 %"
+      const voucherImage = new Image();
+      voucherImage.src = "images/gutschein.jpg";
+      grid.appendChild(voucherImage);
+    }
+  }
+
+  // flip your card
+  function flipCard() {
+    let cardId = this.getAttribute("data-id");
+    cardsChosen.push(cardArray[cardId].name);
+    cardsChosenId.push(cardId);
+    this.setAttribute("src", cardArray[cardId].img);
+    if (cardsChosen.length === 2) {
+      setTimeout(checkForMatch, 500);
+    }
+  }
+
+  //restart game
+  function handleRestart() {
+    for (let i = 0; i < cardArray.length; i++) {
+      const card = document.querySelector(".card");
+      grid.removeChild(card);
+    }
+    cardsWon = [];
+    cardsChosen = [];
+    cardsChosenId = [];
+  
+    resultDisplay.textContent = Math.floor(cardsWon.length * 16.66)
+    createBoard();
+    gameText.textContent = "Start playing!";
+  }
+
+  createBoard();
+  restartBtn.addEventListener("click", handleRestart);
+});
